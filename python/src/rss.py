b62b1c681c11db955caee4d93881bd73f7e4bb15
Edward <none@stream.lan>
2020-05-15 14:50:59 +0100

    Can now read feeds

--- python/src/rss.py
+++ python/src/rss.py
@@ -1,3 +1,50 @@
+import feedparser
+import logging
+import datetime
+import time
+
+FEED_LIST = '/data/feeds'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
+
+def parse_time(struct_time):
+    t = time.mktime(struct_time)
+    return datetime.datetime.fromtimestamp(t)
+
+def get_feed_urls():
+    try:
+        with open(FEED_LIST, 'r') as f:
+            return f.readlines()
+    except:
+        logger.exception("Can't read feed file")
+        return []
+
+def process_url(url):
+    url = url.strip()
+    logger.debug("Starting parsing of '{}'".format(url))
+    d = feedparser.parse(url)
+    feed = d.feed
+    title = feed.title
+    logger.debug("Feed title '{}'".format(title))
+    entries = d.entries
+    logger.debug("Found {} entries".format(len(entries)))
+    x = [process_entry(e) for e in entries]
+
+def process_entry(entry):
+    title = entry.title
+    summary = entry.summary
+    dt = parse_time(entry.published_parsed)
+    url = entry.link
+    logger.debug("{} was published on {} (available at {})".format(title, dt, url))
+    logger.debug(summary[:100])
+
+
 
 def main():
-    print("Hello world")
+    logger.info("Hello world")
+    feed_urls = get_feed_urls()
+    feed_objs = [process_url(url) for url in feed_urls]
+    logger.info("Done")
